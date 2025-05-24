e2e003b4164e494b261c86a031f370dfe19f0a17
OsssGhoul <osamalghoul2@gmail.com>
2025-05-24 12:03:42 +0300

    added meta data

--- lib/resend.ts
+++ lib/resend.ts
@@ -6,9 +6,11 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (name: string, email: string, message: string) => {
     await resend.emails.send({
-        to: 'osamalghoul2@gmail.com',
-        from: `${name} <${email}>`,
+        to: 'info@arabian-industry.com',
+        from: `UPAL <onboarding@resend.dev>`,
         subject: 'New message from UPAL website',
-        html: `<p>${message}</p>`,
+        html: `<p>Hello my name is: ${name} <br />
+        Gmail: ${email} <br />
+         ${message}</p>`,
     });
 }
